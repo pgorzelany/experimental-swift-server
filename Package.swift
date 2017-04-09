74aff7586f1630b2d217b836793aa7c38d3f2b14
pgorzelany <piotr.gorzelany@gmail.com>
2017-04-09 20:08:27 +0200

    Added swifty gpio

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "experimental-swift-server",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 0, minor: 8)
     ],
     exclude: [
         "Config",
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,13 +1,29 @@
 import Vapor
+import SwiftyGPIO
 
 let drop = Droplet()
+//let gpios = SwiftyGPIO.GPIOs(for: .RaspberryPi2)
+//guard let ledGpio = gpios[GPIOName.P27] else {
+//    fatalError("Could not initialize gpio pin")
+//}
+//ledGpio.direction = .OUT
 
 drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+    return "To jest vapor"
 }
 
-drop.resource("posts", PostController())
+drop.get("led", "turn_on") { (request) in
+//    ledGpio.value = 1
+    return "The led should turn on"
+}
+
+drop.get("led", "turn_off") { (request) in
+//    ledGpio.value = 0
+    return "The led should turn off"
+}
+
+drop.get("post", Int.self) { (request, postNumber) in
+    return "This is the post site for post number \(postNumber)"
+}
 
 drop.run()
