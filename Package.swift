d4c8c0514b9d992343cbc27b8c6a88a42d902ece
pgorzelany <piotr.gorzelany@gmail.com>
2017-04-09 18:50:56 +0200

    Fixed Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "test",
+    name: "experimental-swift-server",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
     ],
