48fbcc7690904a8c22b0f9501021bb890c6fe8f0
PiotrGorzelany-RST <piotr.gorzelany@rst-it.com>
2017-04-14 16:26:53 +0200

    Implemented default led and pwm led on the raspberry

--- Sources/App/Utils/Raspberry.swift
+++ Sources/App/Utils/Raspberry.swift
@@ -7,10 +7,37 @@
 //
 
 import Foundation
+import SwiftyGPIO
 
 class Raspberry: RaspberryApi {
     
+    // MARK: Shared instance
+    
     static let shared = Raspberry()
+    
+    // MARK: Properties
+    
+    private let led: Led
+    private let pwmLed: PWMLed
+    
+    // MARK: Lifecycle
+    
+    init(led: Led, pwmLed: PWMLed) {
+        self.led = led
+        self.pwmLed = pwmLed
+    }
+    
+    convenience init() {
+        let gpios = SwiftyGPIO.GPIOs(for: .RaspberryPi2)
+        let ledGPIO = gpios[GPIOName.P0]!
+        let pwms = SwiftyGPIO.hardwarePWMs(for: .RaspberryPi2)!
+        let ledPWM = pwms[0]![GPIOName.P0]!
+        let pwmLed = PWMLed(pwm: ledPWM)
+        let led = Led(gpio: ledGPIO)
+        self.init(led: led, pwmLed: pwmLed)
+    }
+    
+    // MARK: Public methods
     
     func switchLedOn() {
         print("\(#function) called on \(String(describing: type(of: self)))")
